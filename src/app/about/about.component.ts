0e203013ac914a79b900b5ecd5f3cffa2c65fef2
MeenaS <smeena.ece13@gmail.com>
2018-11-07 17:39:02 +0530

    to the github

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -6,8 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
+  
 
-  constructor() { }
+  constructor() {
+    
+   }
 
   ngOnInit() {
   }
