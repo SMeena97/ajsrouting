3b0721b0840121d4c59be459d29efe61064cea3c
MeenaS <smeena.ece13@gmail.com>
2018-11-08 14:22:55 +0530

    to the github

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ModuleWithProviders } from '@angular/core/src/metadata/ng_module';
+
 import { AboutComponent } from './about/about.component';
-import { HeaderComponent } from './header/header.component';
+
 import { GalleryComponent } from './gallery/gallery.component';
 import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
 
-export const AppRoutes: Routes = [
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'gallery', component: GalleryComponent },
   { path: 'contact', component: ContactComponent }
 ];
-export const ROUTING: ModuleWithProviders = RouterModule.forRoot(AppRoutes);
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ModuleWithProviders } from "@angular/core";
+import { FormsModule }    from '@angular/forms';
+import { AppRoutingModule }     from './app-routing.module';
  
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
-import { ROUTING } from './app-routing.module';
+
 import { GalleryComponent } from './gallery/gallery.component';
 import { ContactComponent } from './contact/contact.component';
 
@@ -29,8 +31,9 @@ const rootRouting: ModuleWithProviders = RouterModule.forRoot([], { useHash: tru
   ],
   imports: [
     BrowserModule,
-    rootRouting,
-    ROUTING
+    FormsModule,
+    AppRoutingModule
+    
     
   ],
   providers: [],
